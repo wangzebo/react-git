6981d7c0b5dd45625407138a394f0637f99690b5
zebo <498244595@qq.com>
2021-04-24 21:54:40 +0800

    hebing chongtu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 console.log("main")
 
 class Main {
-  constructor(params) {
-    console.log(params, "123")
-  }
+    constructor(params){
+        console.log(params, "123")
+        console.log('object')
+    }
 }
