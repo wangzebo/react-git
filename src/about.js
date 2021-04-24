9e67b72e6e905c3254419330013a4eccc3e8f7b7
zebo <498244595@qq.com>
2021-04-24 21:00:53 +0800

    style

--- src/about.js
+++ src/about.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 export default class about extends Component {
     render() {
         return (
-            <div>
+            <div style={{display: 'block'}}>
                 about，继续开发
             </div>
         )
