bb8faf0f88725bc1e71a492ae61f27294ff319a3
zebo <498244595@qq.com>
2021-04-24 20:42:58 +0800

    about开发完

--- src/about.js
+++ src/about.js
@@ -1,1 +1,12 @@
 console.log('关于')
+import React, { Component } from 'react'
+
+export default class about extends Component {
+    render() {
+        return (
+            <div>
+                about，继续开发
+            </div>
+        )
+    }
+}
