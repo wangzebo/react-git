826b324f34ecb2063d3c4793121bdb5e810ce242
zebo <498244595@qq.com>
2021-04-24 21:19:28 +0800

    p标签文字

--- src/about.js
+++ src/about.js
@@ -3,6 +3,9 @@ import React, { Component } from "react"
 
 export default class about extends Component {
   render() {
-    return <div style={{ display: "block", padding: "5px" }}>about，继续开发..</div>
+    return <div style={{ display: "block", padding: "5px" }}>
+        <p>about，继续开发..</p>
+        
+        </div>
   }
 }
