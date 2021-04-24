55174981f40c897ceb37fd668f8d0fb0f791e478
jianglinghua <7656314@qq.com>
2021-04-24 21:39:37 +0800

    1

--- src/works.js
+++ src/works.js
@@ -3,6 +3,8 @@ import React, { Component } from "react"
 console.log("b")
 console.log("d")
 
+console.log("1")
+
 export default class works extends Component {
   render() {
     return <div>works</div>
